dafc1e0848bb2c92a1a4de1c39bfa28834ea99c5
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-03-07 21:28:27 +0500

    .

--- app/blogtitle/page.tsx
+++ app/blogtitle/page.tsx
@@ -76,6 +76,16 @@ export default function BlogTitle() {
             </div>
         </div>
       </div> */}
+      <div className="container">
+        <div className="card offset col-8">
+            <div className="card-header">
+                <h3>Title</h3>
+            </div>
+            <div className="card-body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laboriosam, asperiores assumenda dolorem labore explicabo suscipit aspernatur ut nostrum reiciendis repellat doloribus a alias reprehenderit cumque odio distinctio culpa. Commodi.</p>
+            </div>
+        </div>
+      </div>
         { <div className="container">
             {blogs.map(blog => (
                 <div className="card" key={blog._id}>
